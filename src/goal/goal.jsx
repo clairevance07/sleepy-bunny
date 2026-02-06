9976207ca068e9cba5d9493a6bd29ae02b2e17a1
clairevance07 <clairevance07@gmail.com>
2026-02-06 16:43:52 -0700

    Created view components and moved files into the correct folders.

---
+++ src/goal/goal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Goal() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>goal displayed here</div>
+    </main>
+  );
+}
---
+++ src/weather/weather.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Weather() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>weather displayed here</div>
+    </main>
+  );
+}
