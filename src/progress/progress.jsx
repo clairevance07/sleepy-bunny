ebf163d319d5fd416b4cd317983cf88182af78fa
clairevance07 <clairevance07@gmail.com>
2026-02-23 16:39:08 -0700

    Added popup for editing daily progress

--- src/progress/progress.jsx
+++ src/progress/progress.jsx
@@ -15,50 +15,47 @@ const getPastDays = () => {
 export function Progress() {
 
   const daysList = getPastDays();
-  
+  const goal = Number(localStorage.getItem('sleepGoal')) || 8;
+  const [sleepLogs, setSleepLogs] = React.useState(() => {
+    return JSON.parse(localStorage.getItem('sleepLog')) || {};
+  });
+  const [selectedDate, setSelectedDate] = React.useState(null);
 
   return (
         <>
         <NavLink id="exit" to="../track">✖️</NavLink>
         <h2 id="overview">Progress</h2>
         <div className="container">
-            <div className="day-header">Sun</div>
-            <div className="day-header">Mon</div>
-            <div className="day-header">Tue</div>
-            <div className="day-header">Wed</div>
-            <div className="day-header">Thu</div>
-            <div className="day-header">Fri</div>
-            <div className="day-header">Sat</div>
-            <div className="card">1</div>
-            <div className="card">2</div>
-            <div className="card">3</div>
-            <div className="card">4</div>
-            <div className="card">5</div>
-            <div className="card">6</div>
-            <div className="card">7</div>
-            <div className="card">8</div>
-            <div className="card">9</div>
-            <div className="card">10</div>
-            <div className="card">11</div>
-            <div className="card">12</div>
-            <div className="card">13</div>
-            <div className="card">14</div>
-            <div className="card">15</div>
-            <div className="card">16</div>
-            <div className="card">17</div>
-            <div className="card">18</div>
-            <div className="card">19</div>
-            <div className="card">20</div>
-            <div className="card">21</div>
-            <div className="card">22</div>
-            <div className="card">23</div>
-            <div className="card">24</div>
-            <div className="card">25</div>
-            <div className="card">26</div>
-            <div className="card">27</div>
-            <div className="card">28</div>
+            {daysList.map((dateString) => {
+              const hours = sleepLogs[dateString]
+
+              let status = "empty";
+
+              if (hours !== undefined) {
+                if (hours < goal) {
+                  status = "under"
+                }
+                else {
+                  status = "reached"
+                }
+              }
+
+              return (
+                <div className={`card ${status}`} onClick={() => setSelectedDate(dateString)}>{dateString}</div>
+              );
+            })}
         </div>
         <div id="streak">Streak: 1</div>
+
+        {selectedDate && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <h2 id="progress-header">{selectedDate}</h2>
+            <button id="button" onClick={() => setSelectedDate(null)}>✖️</button>
+            <input className="form-control" id="start" type="number" placeholder="hours"></input>
+          </div>
+        </div>
+      )}
     </>
   );
 }
