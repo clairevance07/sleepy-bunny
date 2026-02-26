afdc86e90e2540f85068e5858be3b4c2de0602dc
clairevance07 <clairevance07@gmail.com>
2026-02-25 17:35:56 -0700

    Added notification pop-ups

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -9,6 +9,7 @@ export function Friends() {
     });
 
     const [friendCode, setFriendCode] = React.useState("");
+    const [notifications, setNotifications] = React.useState([]);
 
     React.useEffect(() => {
         localStorage.setItem('userFriends', JSON.stringify(friendsList));
@@ -49,8 +50,23 @@ export function Friends() {
         }
     }
 
+    const sendHighFive = (name) => {
+        const id = Date.now();
+        const newNote = { id: id, text: `You sent a high five to ${name}!`};
+        setNotifications(prev => [newNote, ...notifications].slice(0, 5));
+        setTimeout(() => {
+            setNotifications(prev => prev.filter(note => note.id !== id));
+        }, 5000);
+    }   
+
     return (
-        <div>
+        <div className="friends-container">
+            <div className="friends-sidebar">
+                <h3 className="sidebar-title">Activity</h3>
+                <div className="notifications">{notifications.length === 0 ? (<p className="empty-msg">Your inbox is empty!</p>)
+                : (notifications.map((note) => (<div key={note.id} className="notification-item">{note.text}</div>)))}</div>
+            </div>
+
         <div className="friends-page">
             <div className = "friends-grid">
                 {friendsList.map((friend, index) => (
@@ -58,7 +74,7 @@ export function Friends() {
                         <div className="bunny">🐰</div>
                         <div className="friend-name">{friend.name}</div>
                         <div className="streak">Streak: {friend.streak}</div>
-                        <button type="submit" className="btn send">🙌</button>
+                        <button type="submit" className="btn send" onClick={() => sendHighFive(friend.name)}>🙌</button>
                     </div>  
                 ))}
             </div>
@@ -68,6 +84,7 @@ export function Friends() {
             <input className="form-control" id="field" type="text" placeholder="Insert friend code" value={friendCode} onChange={(e) => setFriendCode(e.target.value)}></input>
             <button onClick={handleAddFriends} type="button" className="btn send" id="add">Add friend</button>
         </div>   
+
     </div>
   );
 }
